7f05d1b65da8302b0b44f3138534991b218e76a5
Naved Rangwala <narangwa@microsoft.com>
2022-10-09 21:52:11 -0700

    gave a way to skip the renderer to let the consumer mount their own renderer

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export { useUIUtilContext, UIUtilContext } from "./ui-utils/UIUtilProvider";
+export { UIUtilRenderer } from "./ui-utils/UIUtilRenderer";
 export * from "./ui-utils/uiUtil.interface";
 export { DryUXProvider } from "./provider";
 export * from "./helpers/utilities";
--- src/provider.tsx
+++ src/provider.tsx
@@ -2,9 +2,9 @@ import * as React from "react";
 import { UIUtilProvider } from "./ui-utils/UIUtilProvider";
 import { UIUtilRenderer } from "./ui-utils/UIUtilRenderer";
 
-export const DryUXProvider = React.memo(({ children }) => (
+export const DryUXProvider: React.FC<{ noRenderer?: boolean }> = React.memo(({ children, noRenderer = false }) => (
     <UIUtilProvider>
         {children}
-        <UIUtilRenderer />
+        {!noRenderer && <UIUtilRenderer />}
     </UIUtilProvider>
 ));
