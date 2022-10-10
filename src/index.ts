cc39720d9356aa4560afab47f1d9f48bb048d8ec
Naved Rangwala <narangwa@microsoft.com>
2022-10-09 21:58:19 -0700

    changed export names

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-export { useUIUtilContext, UIUtilContext } from "./ui-utils/UIUtilProvider";
-export { UIUtilRenderer } from "./ui-utils/UIUtilRenderer";
+export { useUIUtilContext as useDryUxContext, UIUtilContext as DryUXContext } from "./ui-utils/UIUtilProvider";
+export { UIUtilRenderer as DryUXRenderer } from "./ui-utils/UIUtilRenderer";
 export * from "./ui-utils/uiUtil.interface";
 export { DryUXProvider } from "./provider";
 export * from "./helpers/utilities";
