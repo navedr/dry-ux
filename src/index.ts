c475611b87fe63c29ceefa101532f58ac5cf3aa2
Naved Rangwala <narangwa@microsoft.com>
2022-10-09 21:43:52 -0700

    added exports

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
-export { useUIUtilContext } from "./ui-utils/UIUtilProvider";
+export { useUIUtilContext, UIUtilContext } from "./ui-utils/UIUtilProvider";
+export * from "./ui-utils/uiUtil.interface";
 export { DryUXProvider } from "./provider";
 export * from "./helpers/utilities";
