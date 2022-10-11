bc446f345c4932303b46c76073bfd9b809722268
Naved Rangwala <narangwa@microsoft.com>
2022-10-11 09:41:18 -0700

    added children as prop in type

--- src/provider.tsx
+++ src/provider.tsx
@@ -2,7 +2,16 @@ import * as React from "react";
 import { UIUtilProvider } from "./ui-utils/UIUtilProvider";
 import { UIUtilRenderer } from "./ui-utils/UIUtilRenderer";
 
-export const DryUXProvider: React.FC<{ noRenderer?: boolean }> = React.memo(({ children, noRenderer = false }) => (
+interface IDryUXProviderProps {
+    /**
+     * Does not render the renderer for this provider. If this is set to true, then the renderer needs to be mounted
+     * by explicitly.
+     */
+    noRenderer?: boolean;
+    children: React.ReactNode;
+}
+
+export const DryUXProvider: React.FC<IDryUXProviderProps> = React.memo(({ children, noRenderer = false }) => (
     <UIUtilProvider>
         {children}
         {!noRenderer && <UIUtilRenderer />}
