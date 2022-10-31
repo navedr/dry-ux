846dacf0d0179e8ee402d7df940d4e1bc54abbdb
Naved Rangwala <narangwa@microsoft.com>
2022-10-31 16:44:56 -0700

    added classSet

--- src/helpers/classSet.js
+++ src/helpers/classSet.js
@@ -15,17 +15,17 @@
  */
 
 export const classSet = classNames => {
-    var names = "";
+    let names = "";
 
     if (typeof classNames == "object") {
-        for (var name in classNames) {
+        for (let name in classNames) {
             if (!classNames.hasOwnProperty(name) || !classNames[name]) {
                 continue;
             }
             names += name + " ";
         }
     } else {
-        for (var i = 0; i < arguments.length; i++) {
+        for (let i = 0; i < arguments.length; i++) {
             // We should technically exclude 0 too, but for the sake of backward
             // compat we'll keep it (for now)
             if (arguments[i] == null) {
