3c4c5171427df8d6b7aa88e932850021b0e1aa0d
Naved Rangwala <narangwa@microsoft.com>
2023-06-30 19:57:30 -0700

    fixed error screen

--- src/error/ErrorScreen.tsx
+++ src/error/ErrorScreen.tsx
@@ -5,8 +5,8 @@ export const ErrorScreen = React.memo(() => (
         <h1 style={{ fontSize: 70 }}>
             <i className="fa fa-exclamation-triangle"></i>
         </h1>
-        <h1>Oop, Something went wrong!</h1>
-        <h4 className="pull-center" style={{ width: 500, maxWidth: "100%", lineHeight: 1.5 }}>
+        <h1>Oops, Something went wrong!</h1>
+        <h4 className="pull-center" style={{ width: 500, maxWidth: "90vw", lineHeight: 1.5 }}>
             Try to refresh this page or feel free to contact us if the problem persists.
         </h4>
         <a href="/" className="btn btn-primary mtop mbm">
