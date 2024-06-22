2f14bafde7b540088e0b9bc27d822602418ec6d1
Naved Rangwala <narangwa@microsoft.com>
2024-06-22 13:36:12 -0700

    changed error boundary

--- src/error/ErrorBoundary.tsx
+++ src/error/ErrorBoundary.tsx
@@ -1,24 +1,65 @@
 import * as React from "react";
 import { ErrorScreen } from "./ErrorScreen";
 
-export class ErrorBoundary extends React.Component<
-    { fallback?: JSX.Element; children: JSX.Element },
-    { hasError: boolean }
-> {
-    constructor(props) {
+export interface IErrorBoundaryProps {
+    errorHandler?: (error?: Error, info?: React.ErrorInfo) => void;
+    onErrorCallback?: (error?: Error) => void;
+    rethrowError?: boolean;
+    fallbackUI?: React.ReactNode | ((error: Error, resetError: () => void) => JSX.Element);
+}
+
+interface IErrorBoundaryInnerState {
+    error: Error | null;
+}
+
+export class ErrorBoundary extends React.PureComponent<IErrorBoundaryProps, IErrorBoundaryInnerState> {
+    constructor(props: IErrorBoundaryProps) {
         super(props);
-        this.state = { hasError: false };
+        this.state = { error: null };
     }
 
-    static getDerivedStateFromError(error) {
-        return { hasError: true };
+    public static getDerivedStateFromError(error: Error) {
+        return { error };
     }
 
-    render() {
-        if (this.state.hasError) {
-            return this.props.fallback || <ErrorScreen />;
+    public componentDidCatch(error: Error, info: React.ErrorInfo) {
+        this.handleError(error, info);
+    }
+
+    public render = () => {
+        const { children, fallbackUI = null } = this.props;
+        const { error } = this.state;
+        if (error) {
+            if (typeof fallbackUI === "function") {
+                return fallbackUI(error, this.resetError.bind(this));
+            }
+            return fallbackUI || <ErrorScreen />;
         }
 
-        return this.props.children;
+        return children;
+    };
+
+    private handleError = (error: Error, info: React.ErrorInfo) => {
+        const { errorHandler, onErrorCallback, rethrowError } = this.props;
+
+        if (onErrorCallback) {
+            onErrorCallback(error);
+        }
+
+        if (errorHandler) {
+            errorHandler(error, info);
+            if (rethrowError) {
+                throw error;
+            }
+            return;
+        }
+
+        if (rethrowError) {
+            throw error;
+        }
+    };
+
+    private resetError() {
+        this.setState({ error: null });
     }
 }
